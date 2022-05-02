2d3db4c9fa013598a75870bbc9d4074a79995db4
AscendedSion <80121670+AscendedSion@users.noreply.github.com>
2022-05-02 19:38:57 +0100

    Add files via upload

---
+++ Pizzaeaters/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Pizzaeaters
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.process1 = new System.Diagnostics.Process();
+            this.Yes = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardInputEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // Yes
+            // 
+            this.Yes.Location = new System.Drawing.Point(428, 0);
+            this.Yes.Name = "Yes";
+            this.Yes.Size = new System.Drawing.Size(375, 208);
+            this.Yes.TabIndex = 0;
+            this.Yes.Text = "Yes";
+            this.Yes.UseVisualStyleBackColor = true;
+            this.Yes.Click += new System.EventHandler(this.Yes_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(428, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(375, 241);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "No";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(6, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 65);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Do YOU Eat Pizza?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 432);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "You have H.I.V, LOL! Owned.";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(66, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(97, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Initialize RAT?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 458);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Yes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Do YOU Eat Pizza?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Yes;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        internal System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Pizzaeaters/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Media;
+using System.Net;
+using System.Net.Http;
+
+namespace Pizzaeaters
+{
+    public partial class Form1 : Form
+    {
+        private object eyoip;
+        private object streamreader;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Yes_Click(object sender, EventArgs e)
+        {
+            string message = "Well done! You're safe. Eat your pizza happily.";
+            string title = "Good Answer";
+            MessageBox.Show(message, title);
+           // Process.Start("cmd.exe", @"/C taskkill /IM cmd.exe");
+            Process.Start("cmd.exe", @"/c taskkill /im Pizzaeaters.exe");
+           // Process.Start("cmd.exe", @"/C taskkill /IM cmd.exe");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad");
+            Process.Start("cmd", @"/C echo Nice house, it looks like i have planted several IED's inside of it. However, if you find them in time (60 seconds) Nothing will happen. I live in your walls.");
+            Process.Start("calc", @"/C taskkill /IM notepad.exe");
+            Process.Start("cmd.exe", @"/C ipconfig /all");
+            Process.Start("cmd.exe", @"/C 	control netconnections");
+            Process.Start("cmd.exe", @"/C route");
+            Process.Start("cmd.exe", @"/C 	netstat");
+            Process.Start("cmd.exe", @"/C ipconfig");
+            Console.WriteLine("Initializing RAT Trojan");
+            //Process.Kill("");
+
+            var endpoint = "https://api.ipify.org/";
+            using (var client = new HttpClient())
+            {
+                var externalIP = client.GetStringAsync(endpoint).Result;
+                //Console.WriteLine($"external IP {externalIP}");
+
+                // eyoip.Text = externalIP;
+
+                string message = "Monster. You deserve whats coming to you.";
+                string title = "AscendedSION";
+                MessageBox.Show(message, title);
+                Process.Start("cmd.exe", @"/c taskkill /im Pizzaeaters.exe");
+                    
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string message = "RAT Initialized.";
+            //string title = "AscendedSION";
+            MessageBox.Show(message);
+            Process.Start("cmd.exe", @"/C taskkill /IM cmd.exe");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
